c6bc5f965de1f2368f8678fa1a5a052818dbf5eb
anasarifin <anasarifin1995@gmail.com>
2020-03-01 10:55:32 +0700

    score

--- src/screens/student/TestResult.js
+++ src/screens/student/TestResult.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import {View, Text, Button} from 'react-native';
 import {CommonActions, StackActions} from '@react-navigation/native';
+import {useDispatch, useSelector} from 'react-redux';
 
 // const resetAction = CommonActions.reset({
 //   index: 0,
 //   routes: [{name: 'login-student'}],
 // });
 
-const studentAdd = props => {
+const TestResult = props => {
+  const question = useSelector(state => state.assessment.assessmentRandom);
+  const choices = useSelector(state => state.assessment.choicesRandom);
+  const answer = useSelector(state => state.assessment.answer);
+  const answerToArray = Object.values(answer);
+  const correct = answerToArray.filter(x => x === 1).length;
+  const score = (100 / answerToArray.length) * correct;
+
   return (
     <View>
       <Text>THIS IS STUDENT FINISH SCREEN</Text>
+      <Text>Total Question: {answerToArray.length}</Text>
+      <Text>Correct Answer: {correct}</Text>
+      <Text>Score: {score}</Text>
       <Button
         title="Back to home"
         onPress={() => props.navigation.navigate('student-home')}
@@ -19,4 +30,4 @@ const studentAdd = props => {
   );
 };
 
-export default studentAdd;
+export default TestResult;
